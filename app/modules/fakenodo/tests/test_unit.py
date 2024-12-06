44b4d73dbad2694aef9e3936a006fefa518bd2da
Jose Maria Baquero Rodriguez <josbaqrod@alum.us.es>
2024-12-06 13:59:03 +0100

    test: Añadidos nuevos test para los métodos de fakenodo

--- app/modules/fakenodo/tests/test_unit.py
+++ app/modules/fakenodo/tests/test_unit.py
@@ -2,9 +2,12 @@ import pytest
 from unittest.mock import MagicMock, patch
 from app.modules.fakenodo.services import FakenodoService
 from app.modules.fakenodo.models import Deposition
+from app.modules.dataset.models import DataSet
 from app import create_app
 from app import db
 from sqlalchemy import inspect
+
+from app.modules.featuremodel.models import FeatureModel
 
 
 @pytest.fixture
@@ -68,3 +71,45 @@ def test_create_new_deposition(fakenodo_service, app):
             
             assert result["id"] == 1
             assert result["message"] == "Deposition succesfully created in Fakenodo"
+            
+
+def test_upload_file(fakenodo_service, app):
+    with app.app_context():
+        mock_dataset = MagicMock(spec=DataSet)
+        mock_feature_model = MagicMock(spec=FeatureModel)
+        mock_feature_model.fm_meta_data.uvl_filename = "test_file.uvl"
+        mock_user = MagicMock()
+        mock_user.id = 1
+
+        # Ya no es necesario el mock del checksum
+        with patch("os.path.getsize", return_value=100):
+            result = fakenodo_service.upload_file(mock_dataset, 1, mock_feature_model, user=mock_user)
+            assert result["id"] == 1
+            assert result["file"] == "test_file.uvl"
+            assert result["fileSize"] == 100
+            assert result["message"] == "File Uploaded to deposition with id 1"
+
+
+def test_publish_deposition(fakenodo_service, app, setup_deposition):
+    result = fakenodo_service.publish_deposition(setup_deposition.id)
+    assert result["status"] == "published"
+    assert result["conceptdoi"] == f"fakenodo.doi.{setup_deposition.id}"
+    assert result["message"] == "Deposition published successfully in fakenodo."
+
+
+def test_get_deposition(fakenodo_service, app, setup_deposition):
+    result = fakenodo_service.get_deposition(setup_deposition.id)
+    assert result["id"] == setup_deposition.id
+    assert result["metadata"]["title"] == "Test Deposition"
+    assert result["status"] == "draft"
+
+
+def test_get_doi(fakenodo_service, app):
+    with app.app_context():
+        mock_deposition = MagicMock(spec=Deposition)
+        mock_deposition.id = 1
+        mock_deposition.doi = "fakenodo.doi.1"
+
+        with patch.object(fakenodo_service, 'get_deposition', return_value={"doi": "fakenodo.doi.1"}):
+            result = fakenodo_service.get_doi(1)
+            assert result == "fakenodo.doi.1"
