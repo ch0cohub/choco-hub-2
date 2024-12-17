4a2ba4dc93ddd61d4fc39adb566081a4fd7fc4ae
Alfonso Luis Alonso Lanzaran <alfalolan@alum.us.es>
2024-12-17 14:59:09 +0100

    fix(hubfile_test): Arreglado el test para que pase en los wf

--- app/modules/hubfile/tests/test_unit_edit_uvl.py
+++ app/modules/hubfile/tests/test_unit_edit_uvl.py
@@ -1,8 +1,11 @@
 import pytest
+import os
+import shutil
 from app import db
 from app.modules.dataset.models import DataSet, DSMetaData, PublicationType
 from app.modules.featuremodel.models import FeatureModel, FMMetaData
 from app.modules.hubfile.models import Hubfile
+from dotenv import load_dotenv
 
 
 def test_edit_not_found(test_client):
@@ -19,7 +22,7 @@ def test_edit_file_success(test_client):
 
     # New DSMetaData y DataSet for testing
     dsmetadata = DSMetaData(
-        id=3,
+        id=99,
         title="Test Dataset",
         description="Test Description",
         publication_type=PublicationType.JOURNAL_ARTICLE,
@@ -27,12 +30,12 @@ def test_edit_file_success(test_client):
     db.session.add(dsmetadata)
     db.session.commit()
 
-    dataset = DataSet(id=1, user_id=1, ds_meta_data_id=dsmetadata.id)
+    dataset = DataSet(id=99, user_id=1, ds_meta_data_id=dsmetadata.id)
     db.session.add(dataset)
     db.session.commit()
     # New FMMetaData for testing
     fmmetadata = FMMetaData(
-        id=3,
+        id=99,
         uvl_filename="file1.uvl",
         title="Feature Model 1",
         description="Description for feature model 1",
@@ -46,16 +49,34 @@ def test_edit_file_success(test_client):
     )
     db.session.add(feature_model)
     db.session.commit()
+
+    #Creación del archivo para editar
+    existe = os.path.exists("uploads/user_1/dataset_99/uvl_test.uvl")
+    working_dir = os.getenv("WORKING_DIR", "")
+    src_folder = os.path.join(
+        working_dir, "app", "modules", "dataset", "uvl_examples"
+    )
+    dest_folder = os.path.join(
+                working_dir, "uploads", f"user_{dataset.user_id}", f"dataset_{dataset.id}"
+            )
+    file_name = "uvl_test.uvl"
+    file_path = os.path.join(dest_folder, file_name)
+
+    if existe is False:
+        load_dotenv()
+        os.makedirs(dest_folder, exist_ok=True)
+        shutil.copy(os.path.join(src_folder, file_name), dest_folder)
+
     # Create a new hubfile for testing
     new_hubfile = Hubfile(
-        id=1,
-        name="file1.uvl",
+        id=99,
+        name=file_name,
         checksum="1234567890abcdeg",
-        size=1024,
+        size=os.path.getsize(file_path),
         feature_model_id=feature_model.id,
     )
     db.session.add(new_hubfile)
-    db.session.commit
+    db.session.commit()
 
     """
     Realiza un POST a /file/edit/1 y verifica que el POST ha sido efectuado correctamente.
@@ -83,10 +104,10 @@ def test_edit_file_success(test_client):
     }
 
     # Realizar el POST para editar el archivo
-    response = test_client.post("/file/edit/1", json=edit_payload)
+    response = test_client.post("/file/edit/99", json=edit_payload)
     assert response.status_code == 200, "El POST de edición no fue exitoso."
 
     # Verificar que el contenido ha sido actualizado
-    response = test_client.get("/file/view/1")
+    response = test_client.get("/file/view/99")
     assert response.status_code == 200, "No se pudo obtener el archivo editado."
     assert b"EDITADO" in response.data, "El contenido del archivo no fue actualizado correctamente."
